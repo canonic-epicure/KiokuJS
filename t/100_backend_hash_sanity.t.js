6f53c72c87cdd4b2b277e6269643c0af83269b53
nickolay <nickolay@nickolay-note.(none)>
2010-07-15 16:25:52 +0400

    - passing the whole test suite again

--- t/100_backend_hash_sanity.t.js
+++ t/100_backend_hash_sanity.t.js
@@ -1,90 +1,85 @@
 StartTest(function(t) {
     
+    //======================================================================================================================================================================================================================================================
+    t.diag('Sanity')
+    
+    t.ok(KiokuJS.Backend.Hash, "KiokuJS.Backend.Hash is here")
+    t.ok(KiokuJS.Node, "KiokuJS.Node is here")
+    t.ok(KiokuJS.Resolver.Standard, "KiokuJS.Resolver.Standard is here")
+    
+    var resolver = new KiokuJS.Resolver.Standard()
+    
+    t.ok(resolver, "KiokuJS.Resolver.Standard was instantiated")
+    
+    var backend = new KiokuJS.Backend.Hash({
+        resolver : resolver
+    })
+    
+    t.ok(backend, "KiokuJS.Backend.Hash was instantiated")
+    
+    
+    //======================================================================================================================================================================================================================================================
+    t.diag('Insert')
+    
     var async0 = t.beginAsync()
     
-    use([ 'KiokuJS.Backend.Hash' ], function () {
+    backend.insert([
+    
+        new KiokuJS.Node({ 
+            ID          : 1,
+            
+            className   : 'Object',
+            
+            resolver     : resolver,
+            
+            data        : { foo : "foo1" } 
+        }),
         
-        //======================================================================================================================================================================================================================================================
-        t.diag('Sanity')
-        
-        t.ok(KiokuJS.Backend.Hash, "KiokuJS.Backend.Hash is here")
-        t.ok(KiokuJS.Node, "KiokuJS.Node is here")
-        t.ok(KiokuJS.Resolver.Standard, "KiokuJS.Resolver.Standard is here")
-        
-        var resolver = new KiokuJS.Resolver.Standard()
-        
-        t.ok(resolver, "KiokuJS.Resolver.Standard was instantiated")
-        
-        var backend = new KiokuJS.Backend.Hash({
-            resolver : resolver
+        new KiokuJS.Node({ 
+            ID          : 2, 
+            
+            className   : 'Object',
+            
+            resolver     : resolver,
+            
+            data        : { bar : "foo2" } 
         })
         
-        t.ok(backend, "KiokuJS.Backend.Hash was instantiated")
-        
+    ]).andThen(function () {
         
         //======================================================================================================================================================================================================================================================
-        t.diag('Insert')
+        t.diag('Exists')
         
         
-        
-        backend.insert([
-        
-            new KiokuJS.Node({ 
-                ID          : 1,
-                
-                className   : 'Object',
-                
-                typeMap     : resolver.resolveSingle('Object'),
-                
-                data        : { foo : "foo1" } 
-            }),
+        backend.exists([ 1, 10, 2 ]).andThen(function (res) {
             
-            new KiokuJS.Node({ 
-                ID          : 2, 
-                
-                className   : 'Object',
-                
-                backend     : backend,
-                
-                data        : { bar : "foo2" } 
-            })
-            
-        ]).andThen(function () {
+            t.ok(res[0], "Entry with ID = 1 exists")
+            t.ok(!res[1], "Entry with ID = 10 doesn't exists")
+            t.ok(res[2], "Entry with ID = 2 exists")
             
             //======================================================================================================================================================================================================================================================
-            t.diag('Exists')
+            t.diag('Get')
             
+            backend.get([ 2, 1, 10 ]).andThen(function (res) {
             
-            backend.exists([ 1, 10, 2 ]).andThen(function (res) {
-                
-                t.ok(res[0], "Entry with ID = 1 exists")
-                t.ok(!res[1], "Entry with ID = 10 doesn't exists")
-                t.ok(res[2], "Entry with ID = 2 exists")
-                
-                //======================================================================================================================================================================================================================================================
-                t.diag('Get')
-                
-                backend.get([ 2, 1, 10 ]).andThen(function (res) {
-                
-                    t.ok(res[0].data.bar == 'foo2', 'Entry with ID = 2, retrieved correctly')
-                    t.ok(res[1].data.foo == 'foo1', 'Entry with ID = 1, retrieved correctly')
-                    t.ok(res[2] == null, 'There is no entry with ID = 10')
+                t.ok(res[0].data.bar == 'foo2', 'Entry with ID = 2, retrieved correctly')
+                t.ok(res[1].data.foo == 'foo1', 'Entry with ID = 1, retrieved correctly')
+                t.ok(res[2] == null, 'There is no entry with ID = 10')
 
-                    
-                    backend.remove([ 1, 2 ]).andThen(function () {
-                    
-                        backend.exists([ 1, 2 ]).andThen(function (res) {
-                            
-                            t.ok(!res[0], "Entry with ID = 1 doesn't exists")
-                            t.ok(!res[1], "Entry with ID = 2 doesn't exists")
-                            
-                            t.done()
-                            
-                            t.endAsync(async0)
-                        })
+                
+                backend.remove([ 1, 2 ]).andThen(function () {
+                
+                    backend.exists([ 1, 2 ]).andThen(function (res) {
+                        
+                        t.ok(!res[0], "Entry with ID = 1 doesn't exists")
+                        t.ok(!res[1], "Entry with ID = 2 doesn't exists")
+                        
+                        t.done()
+                        
+                        t.endAsync(async0)
                     })
                 })
-            }) 
-        })
+            })
+        }) 
     })
 })    
