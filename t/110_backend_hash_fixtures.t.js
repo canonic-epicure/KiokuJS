e212ac95a434cec0c3620aa09b4b3ce5d42b881d
nickolay <nickolay@nickolay-note.(none)>
2010-07-23 11:18:21 +0400

    - simplified fixtures test

--- t/110_backend_hash_fixtures.t.js
+++ t/110_backend_hash_fixtures.t.js
@@ -2,30 +2,25 @@ StartTest(function(t) {
     
     var async0 = t.beginAsync()
     
-    use([ 'KiokuJS.Test', 'KiokuJS.Test.Person', 'KiokuJS.Backend.Hash' ], function () {
+    //======================================================================================================================================================================================================================================================
+    t.diag('Sanity')
+    
+    t.ok(KiokuJS.Test, "KiokuJS.Test is here")
+    t.ok(KiokuJS.Backend.Hash, "KiokuJS.Backend.Hash is here")
+    
+    new KiokuJS.Test({
+        t       : t,
         
-        //======================================================================================================================================================================================================================================================
-        t.diag('Sanity')
+        init    : function () {
+            return KiokuJS.connect({
+                backend : new KiokuJS.Backend.Hash()
+            })
+        }
         
-        t.ok(KiokuJS.Test, "KiokuJS.Test is here")
-        t.ok(KiokuJS.Backend.Hash, "KiokuJS.Backend.Hash is here")
-        t.ok(KiokuJS.Test.Person, "KiokuJS.Test.Person is here")
+    }).runAllFixtures().andThen(function () {
         
-        new KiokuJS.Test({
-            t       : t,
-            
-            init    : function () {
-                return KiokuJS.connect({
-                    backend : new KiokuJS.Backend.Hash()
-                })
-            }
-            
-        }).runAllFixtures().then(function () {
-            
-            t.endAsync(async0)
-            
-            t.done()
-            
-        }).now()
+        t.endAsync(async0)
+        
+        t.done()
     })
 })    
